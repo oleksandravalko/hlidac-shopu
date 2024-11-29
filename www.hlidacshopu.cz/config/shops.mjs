2fb02afc90882101af1c6fabdcf06e73a8c85f0a
Aleš Roubíček <ales@roubicek.name>
2024-11-29 19:45:14 +0100

    Logos fixes

--- www.hlidacshopu.cz/config/shops.mjs
+++ www.hlidacshopu.cz/config/shops.mjs
@@ -3,6 +3,14 @@ import projectPath from "@hckr_/blendid/lib/projectPath.mjs";
 import { shopsArray } from "@hlidac-shopu/lib/shops.mjs";
 import DefaultRegistry from "undertaker-registry";
 
+function dimensions({ viewBox }) {
+  if (!viewBox) return null;
+  const [, , w, h] = viewBox.split(" ");
+  const width = Math.round(Number(w));
+  const height = Math.round(Number(h));
+  return { width, height };
+}
+
 export class ShopsRegistry extends DefaultRegistry {
   constructor(config, pathConfig) {
     super();
@@ -15,12 +23,17 @@ export class ShopsRegistry extends DefaultRegistry {
    */
   init({ task }) {
     task("prepare-shops-data", async () => {
-      const shops = shopsArray().map(x => ({
-        name: x.name,
-        logo: x.logo,
-        url: x.url,
-        show: x.show ?? Boolean(x.viewBox)
-      }));
+      const shops = shopsArray().map(x =>
+        Object.assign(
+          {
+            name: x.name,
+            logo: x.logo,
+            url: x.url,
+            show: x.show ?? Boolean(x.viewBox)
+          },
+          dimensions(x)
+        )
+      );
       const targetPath = projectPath(this.pathConfig.src, this.pathConfig.data.src, "shops.json");
       return writeFile(targetPath, JSON.stringify(shops));
     });
